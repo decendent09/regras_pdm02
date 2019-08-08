96e96a47e4521ca9fe29e40ff96aa2a44c0f62e2
ELTON FEIJO DOS SANTOS <elton.fsantos4@senacsp.edu.br>
2019-08-08 20:37:51 -0300

    Conhecendo regras

---
+++ Regras.js
@@ -0,0 +1,32 @@
+// Exemplo para não se fazer
+// let 1nome= '';
+
+//Exemplo para se fazer
+// string
+let nome = "vinicius";
+
+//Tipo inteiro
+let idade = 20
+
+// Tipo Decimal
+let pwao = 55.25
+
+// Tipo boleano
+
+let visualizador = true;
+
+// Criar constante
+const nome1 = 'vinicius';
+
+// Exibir informações
+console.log('Este é o console.log');
+
+console.log('Exibindo variáveis');
+console.log('nome: ' + nome);
+nome = 'José';
+console.log('nome: ' + nome);
+
+console.log('nome const:' +nome2);
+
+console.log('nome: ' + nome + ' idade: '+idade);
+console.log(`nome: ${nome} idade ${ idade}`)
